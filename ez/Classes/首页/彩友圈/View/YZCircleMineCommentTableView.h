446725929ae829ab72447fca1a1122d34c2c65fd
maowenhao <15346080710@163.com>
2019-07-02 17:26:35 +0800

    彩友圈评论接口

---
+++ ez/Classes/首页/彩友圈/View/YZCircleMineCommentTableView.h
@@ -0,0 +1,17 @@
+//
+//  YZCircleMineCommentTableView.h
+//  ez
+//
+//  Created by dahe on 2019/6/28.
+//  Copyright © 2019 9ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZCircleMineCommentTableView : UITableView
+
+@end
+
+NS_ASSUME_NONNULL_END
